619cf150c16765b30e36913dbc18cad7334cda0d
Niang El Hadji <el-hadji.niang@etu.univ-orleans.fr>
2024-11-12 12:23:53 +0100

    champs sur le site

---
+++ src/main/java/retraite/simulateur/Adherent.java
@@ -0,0 +1,21 @@
+package retraite.simulateur;
+
+import java.util.Date;
+
+public class Adherent {
+    private Date dateNaissance;
+    private String nom;
+    private String prenom;
+    private int sexe;
+    private int ageDepMois;
+    private int nbEnfants;
+    private Date dateRetraiteSouhait;
+    private int SAM;
+    private Boolean trimestrePartage;
+    private int nbEnfantsPartages;
+    private int trimValide;
+    private int trimRequis;
+    private int trimHandicap;
+
+
+}
